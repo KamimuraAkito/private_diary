097b683f26f6e2fe6b81bddada63495f56c03f06
Kamimura Akito <akito0702kamimura@gmail.com>
2021-09-09 05:59:20 +0000

    認証機能・migrate

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-=======
-from django.db import models
->>>>>>> 637cc7e2b20dbc6de5db8ea1220c1b221e785217
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class CustomUser(AbstractUser):
-<<<<<<< HEAD
-=======
-    #拡張ユーザーモデル
->>>>>>> 637cc7e2b20dbc6de5db8ea1220c1b221e785217
 
     class Meta:
         verbose_name_plural = 'CustomUser'
